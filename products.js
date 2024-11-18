c28a67810a4a7916309011422ba584e0fa83418e
Mp15128 <madhuriperala45@gmail.com>
2024-11-18 04:46:47 +0000

    lab submission

---
+++ products.js
@@ -0,0 +1,30 @@
+const fs = require('fs').promises
+const path = require('path')
+
+const productsFile = path.join(__dirname, 'data/full-products.json')
+
+async function list (options = {}) {
+    const { offset = 0, limit = 25, tag } = options;
+    const data = await fs.readFile(productsFile)
+    return JSON.parse(data)
+    .filter(product => {
+        if (!tag) {
+            return product
+        }
+
+        return product.tags.find(( {title} ) => title == tag )
+    })
+    .slice(offset, offset + limit) 
+}
+async function get (id) {
+    const products = await JSON.parse(fs.readFile(productsFile))
+    for(let i=0; i < products.length; i++){
+        if(products[i].id === id){
+            return products[i]
+        }
+    }
+}
+module.exports = {
+    list,
+    get,
+}
